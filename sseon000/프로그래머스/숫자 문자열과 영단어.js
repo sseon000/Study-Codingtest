d03cbfc30996e186fa5d6718c3c2d9b55aa12b0b
bugNick <rlatjsgur000@naver.com>
2023-08-10 23:24:19 +0900

    [프로그래머스] 숫자 문자열과 영단어 / lv1 / 20분 / https://school.programmers.co.kr/learn/courses/30/lessons/81301

---
+++ sseon000/프로그래머스/숫자 문자열과 영단어.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  const match = {
+      'zero' : 0,
+      'one' : 1,
+      'two' : 2,
+      'three' : 3,
+      'four' : 4,
+      'five' : 5,
+      'six' : 6,
+      'seven' : 7,
+      'eight' : 8,
+      'nine' : 9
+  }
+  return +s.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, v => match[v]);
+}
