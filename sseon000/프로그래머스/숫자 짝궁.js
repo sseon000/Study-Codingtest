2d37656c8b852b6ea217f288aba99d7496645d60
bugNick <rlatjsgur000@naver.com>
2024-01-18 14:30:26 +0900

    [프로그래머스] 숫자 짝꿍 / lv1 / 50분 / https://school.programmers.co.kr/learn/courses/30/lessons/131128

--- sseon000/프로그래머스/숫자 짝궁.js
+++ sseon000/프로그래머스/숫자 짝궁.js
@@ -1,5 +1,23 @@
 function solution(X, Y) {
   let answer = [];
+  let arrX = new Array(10).fill(0);
+  X.split('').forEach(el => arrX[el]++);
+  let arrY = new Array(10).fill(0);
+  Y.split('').forEach(el => arrY[el]++);
+
+  arrX.forEach((el, idx) => {
+    if(el != 0 && arrY[idx] != 0) {
+      let min = Math.min(el, arrY[idx]);
+      answer.push(String(idx).repeat(min));
+    }
+  })
+    
+  return answer.length === 0 ? '-1' : Number(answer.join('')) === 0 ? "0" : answer.reverse().join('')
+}
+
+/*=============================== 시간 초과 ==========================
+function solution(X, Y) {
+  let answer = [];
   
   let arrX = X.split('');
   let arrY = Y.split('');
@@ -24,19 +42,23 @@ function solution(X, Y) {
 
   return answer.join('') === '' ? '-1' : answer.join('');
 }
-let X = "100"
-let Y = "2345"
 
-// X = "100"
-// Y = "203045"
+function solution2(X, Y) {
+  let answer = "";
+  let arrX = X.split('').sort((a,b) => b-a);
+  let arrY = Y.split('').sort((a,b) => b-a);
+  console.log(`arrX: ${arrX}`);
+  
+  
+  for(let i=0; i<arrX.length; i++) {
+      if(arrY.includes(arrX[i])) {
+          arrY = [...arrY.splice(0,arrY.indexOf(arrX[i])), ...arrY.splice(arrY.indexOf(arrX[i])+1)]
+          answer += arrX[i];
+      }
 
-// X = "100"
-// Y = "123450"
+      console.log(`arrY: ${arrY}`);
+  }
 
-// X = "12321"
-// Y = "42531"
-
-// X = "5525"
-// Y = "1255"
-
-solution(X,Y);
+  return answer.startsWith('0') ? '0' : answer === "" ? "-1" : answer;
+}
+*/
